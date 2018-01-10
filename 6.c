ec828ddb55d5d949e678717521f0543f3f146b39
SuryaRider <30830554+SuryaRider@users.noreply.github.com>
2018-01-10 16:08:36 +0530

    Update 6.c

--- 6.c
+++ 6.c
@@ -1,27 +1,32 @@
-#include <stdio.h>
-
-int main()
+include<stdio.h>
+#include<conio.h>
+void main()
 {
-    int year;
-
-    printf("Enter a year: ");
-    scanf("%d",&year);
-
-    if(year%4 == 0)
-    {
-        if( year%100 == 0)
-        {
-            // year is divisible by 400, hence the year is a leap year
-            if ( year%400 == 0)
-                printf("%d is a leap year.", year);
-            else
-                printf("%d is not a leap year.", year);
-        }
-        else
-            printf("%d is a leap year.", year );
-    }
-    else
-        printf("%d is not a leap year.", year);
-    
-    return 0;
+int yr;
+printf("Enter the year:");
+scanf("%d",&yr);
+if(yr%4==0)
+{
+if(yr%100==0)
+{
+if(yr%400==0)
+{
+printf("This is a leap year",yr);
+}
+else
+{
+printf("This is not a leap year",yr);
+}
+}
+else
+{
+printf("This is a leap year",yr);
+}
+}
+else
+{
+printf("This is not a leap year",yr);
+}
+}
+getch();
 }

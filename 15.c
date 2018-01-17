1db5c2ccdec00ead13cf66cf3dc506b6d310d629
SuryaRider <30830554+SuryaRider@users.noreply.github.com>
2018-01-17 16:32:32 +0530

    Create 15.c

---
+++ 15.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{  
+    int count,n; 
+    printf("\nEven numbers between 2 intervals");  
+   for(count = 1; count <= 5; count++) 
+   {  
+      
+        if(count%2 == 0) 
+        { 
+            
+           printf("%d ",count); 
+           printf("\n even number between two interval is");
+        }  
+    }  
+   
+    return0;
+    }

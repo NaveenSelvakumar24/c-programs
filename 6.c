799d0dcd73c4d83239752ef55796eb3058efc9b4
SuryaRider <30830554+SuryaRider@users.noreply.github.com>
2018-01-17 15:39:07 +0530

    Update 6.c

--- 6.c
+++ 6.c
@@ -6,11 +6,10 @@ printf("\n enter the year");
 scanf("%d",&a);
 if(year%4==0&&year%400==0)||(year%100!==0)
 {
-printf("it is leap year");
+ printf("it is leap year");
 }
-else
-{
-printf("it is not a leap year");
-}
-getch()
+  else
+  {
+ printf("it is not a leap year");
+  }getch();
 }
